4f34a2400689382df145d52f21d249cb665793cd
Rahul <88mydestiny@gmail.com>
2020-08-06 17:10:47 +0530

    recent courses

--- src/app/pages/recent-courses/recent-courses.component.ts
+++ src/app/pages/recent-courses/recent-courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ContentService } from '../../services/content.service';
+import { Course } from '../../models/course.model';
 
 @Component({
   selector: 'recent-courses',
@@ -7,12 +9,39 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecentCoursesComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  vh: number;
+
+  public recentCourses : Course[];
+  public course1 : Course;
+  public course2 : Course;
+  public course3 : Course;
+  public course4 : Course;
+
+  constructor(private contentService : ContentService) { }  
 
   ngOnInit(): void {
+    this.contentService.getRecentCourses().subscribe(data =>{
+       this.recentCourses = data
+
+       this.course1 = this.recentCourses[0]
+       this.course2 = this.recentCourses[1]
+       this.course3 = this.recentCourses[2]
+       this.course4 = this.recentCourses[3]
+       this.recentCourses.map(data => {
+        let d = data 
+        d.name = data.name.trim()
+        d.description = data.description.trim()
+        return d;
+       } )
+    });
+
+
+  }
+
+  clickMe(){
+    alert('Hi Work in progress')
   }
 
   
-  @Input()
-  vh: number;
 }
